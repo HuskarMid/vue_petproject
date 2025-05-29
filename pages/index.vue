e9fda43bee9532f52ffcd4f35ab26fbe9aa39e64
Arseniy <salychsena@gmail.com>
2025-05-29 12:20:54 +0300

    fix 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { computed } from 'vue'
-import { useUIStore } from './src/shared/model/store/ui'
-import MainPage from './src/pages/MainPage/ui/MainPage.vue'
-import ToggleButton from './src/shared/ui/toggleButton/ToggleButton.vue'
-import PlusButton from './src/shared/ui/PlusButton/PlusButton.vue'
-import Input from './src/shared/ui/Input/Input.vue'
+import { useUIStore } from '@/shared/model/store/ui'
+import MainPage from '@/pages/MainPage/ui/MainPage.vue'
+import ToggleButton from '@/shared/ui/toggleButton/ToggleButton.vue'
+import PlusButton from '@/shared/ui/PlusButton/PlusButton.vue'
+import Input from '@/shared/ui/Input/Input.vue'
 
 const uiStore = useUIStore()
 const theme = computed(() => `theme-${uiStore.getTheme}`)
