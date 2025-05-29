123454752d846cb45b1422c3c5d8feb80eddd273
Arseniy <salychsena@gmail.com>
2025-05-29 11:49:54 +0300

    final changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/vue-petproject/'
+  },
   devtools: { enabled: true },
   modules: [
     '@pinia/nuxt'
